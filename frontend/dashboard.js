569e36bd4ec997c97414a15a581396d9b4e5beec
I-am-Tofunmi <frostycalmen@gmail.com>
2026-04-12 12:25:11 +0100

    save changes

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const API_BASE_URL = "http://127.0.0.1:8000";
+  const API_BASE_URL = window.location.origin;
 
   // Configuration Check
   const token = localStorage.getItem("safeflow_token");
